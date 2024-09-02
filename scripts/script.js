7b43af0faa333bccc49cddf3cc250964b0390fb2
Edson Kichie <102360300+EdsonKichie@users.noreply.github.com>
2024-09-02 08:31:09 -0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,36 @@
+let entrevista1 = {
+    questoes: [
+        {
+            p1: "Qual profissão você pretende exercer?",
+            r1: ""
+        },
+        {
+            p2: "O que você tem feito ou fará para alcançar sua profissão?",
+            r2: ""
+        },
+        {
+            p3: "Quais motivos levaram você a escolher essa profissão?",
+            r3: ""
+        },
+        {
+            p4: "Como você acha que vai estar sua profissão daqui a 10 anos?",
+            r4: ""
+        },
+        {
+            p5: "O que é o sucesso para você e quando acha que o alcançará?",
+            r5: ""
+        },
+        {
+            p6: "Você escolheria o mesmo itinerário formativo? Como isso contribuiria para sua vida profissional?",
+            r6: ""
+        },
+        {
+            p7: "Você acha que a pressão social influencia em suas escolhas acadêmicas?",
+            r7: ""
+        },
+        {
+            p8: "Qual pessoa que mais influenciou em sua decisão profissional?",
+            r8: ""
+        },
+    ]
+}
