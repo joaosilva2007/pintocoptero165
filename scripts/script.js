ccff9a5aa7739c6c58238a5431d96ffd00a1aad0
Edson Kichie <102360300+EdsonKichie@users.noreply.github.com>
2024-09-16 07:55:50 -0300

    Entrevistas js

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,148 @@
 let entrevista1 = {
+    entrevistador: "Ezequiel",
+    entrevistado: "Edson",
     questoes: [
         {
             p1: "Qual profissão você pretende exercer?",
-            r1: ""
+            r1: "Médico" 
         },
         {
             p2: "O que você tem feito ou fará para alcançar sua profissão?",
-            r2: ""
-        },
-        {
-            p3: "Quais motivos levaram você a escolher essa profissão?",
-            r3: ""
+            r2: "Dedicar-se bastante nos estudos"
         },
         {
             p4: "Como você acha que vai estar sua profissão daqui a 10 anos?",
-            r4: ""
+            r4: "Irá saturar e demandará uma maior especialização dos profissionais"
         },
         {
             p5: "O que é o sucesso para você e quando acha que o alcançará?",
-            r5: ""
+            r5: "Carreia estabilizada e mais tempo dedicado à família"
         },
         {
             p6: "Você escolheria o mesmo itinerário formativo? Como isso contribuiria para sua vida profissional?",
-            r6: ""
+            r6: "Sim, apesar de não fazer diferença nenhuma"
         },
         {
             p7: "Você acha que a pressão social influencia em suas escolhas acadêmicas?",
-            r7: ""
+            r7: "Sim"
         },
         {
             p8: "Qual pessoa que mais influenciou em sua decisão profissional?",
-            r8: ""
+            r8: "Familiares"
+        },
+    ]
+}
+let entrevista2 = {
+    entrevistador: "Vinícius",
+    entrevistado: "João",
+    questoes: [
+        {
+            p1: "Qual profissão você pretende exercer?",
+            r1: "Cientista da computação" 
+        },
+        {
+            p2: "O que você tem feito ou fará para alcançar sua profissão?",
+            r2: "Estudar bastante"
+        },
+        {
+            p3: "Quais motivos levaram você a escolher essa profissão?",
+            r3: "Por estar sempre inserido no meio digital"
+        },
+        {
+            p4: "Como você acha que vai estar sua profissão daqui a 10 anos?",
+            r4: "Em 5 anos, aumentará o mercado de trabalho e, em 10 anos, irá saturar e demandará um maior nível técnico"
+        },
+        {
+            p5: "O que é o sucesso para você e quando acha que o alcançará?",
+            r5: "O sucesso será quando eu tiver uma carreia estabilizada e poder ter mais tempo de lazer com a minha família, sem se preocupar com a renda"
+        },
+        {
+            p6: "Você escolheria o mesmo itinerário formativo? Como isso contribuiria para sua vida profissional?",
+            r6: "Sim, porque não faz diferença nenhuma"
+        },
+        {
+            p7: "Você acha que a pressão social influencia em suas escolhas acadêmicas?",
+            r7: "Sim"
+        },
+        {
+            p8: "Qual pessoa que mais influenciou em sua decisão profissional?",
+            r8: "Familiares"
+        },
+    ]
+}
+let entrevista3 = {
+    entrevistador: "Edson",
+    entrevistado: "Ezequiel",
+    questoes: [
+        {
+            p1: "Qual profissão você pretende exercer?",
+            r1: "Engenheiro aeroespacial" 
+        },
+        {
+            p2: "O que você tem feito ou fará para alcançar sua profissão?",
+            r2: "Estudar bastante"
+        },
+        {
+            p3: "Quais motivos levaram você a escolher essa profissão?",
+            r3: "Por estar sempre inserido no meio e sempre gostar"
+        },
+        {
+            p4: "Como você acha que vai estar sua profissão daqui a 10 anos?",
+            r4: "Aumentará muito a demanda no exterior e exigirá de mais pessoas especializadas na área"
+        },
+        {
+            p5: "O que é o sucesso para você e quando acha que o alcançará?",
+            r5: "O sucesso será quando eu tiver uma carreia estabilizada e poder ter mais tempo de lazer com a minha família, sem se preocupar com a renda e poder financiar meus próprios projetos"
+        },
+        {
+            p6: "Você escolheria o mesmo itinerário formativo? Como isso contribuiria para sua vida profissional?",
+            r6: "Sim, porque não faz diferença nenhuma"
+        },
+        {
+            p7: "Você acha que a pressão social influencia em suas escolhas acadêmicas?",
+            r7: "Não"
+        },
+        {
+            p8: "Qual pessoa que mais influenciou em sua decisão profissional?",
+            r8: "Eu mesmo"
+        },
+    ]
+}
+let entrevista4 = {
+    entrevistador: "João",
+    entrevistado: "Vinícus",
+    questoes: [
+        {
+            p1: "Qual profissão você pretende exercer?",
+            r1: "Cientista da computação" 
+        },
+        {
+            p2: "O que você tem feito ou fará para alcançar sua profissão?",
+            r2: "Estudar bastante"
+        },
+        {
+            p3: "Quais motivos levaram você a escolher essa profissão?",
+            r3: "Por estar sempre inserido no meio digital"
+        },
+        {
+            p4: "Como você acha que vai estar sua profissão daqui a 10 anos?",
+            r4: "Em 5 anos, aumentará o mercado de trabalho e, em 10 anos, irá saturar e demandará um maior nível técnico"
+        },
+        {
+            p5: "O que é o sucesso para você e quando acha que o alcançará?",
+            r5: "O sucesso será quando eu tiver uma carreia estabilizada e poder ter mais tempo de lazer com a minha família, sem se preocupar com a renda"
+        },
+        {
+            p6: "Você escolheria o mesmo itinerário formativo? Como isso contribuiria para sua vida profissional?",
+            r6: "Sim, porque não faz diferença nenhuma"
+        },
+        {
+            p7: "Você acha que a pressão social influencia em suas escolhas acadêmicas?",
+            r7: "Sim"
+        },
+        {
+            p8: "Qual pessoa que mais influenciou em sua decisão profissional?",
+            r8: "Familiares"
         },
     ]
 }
